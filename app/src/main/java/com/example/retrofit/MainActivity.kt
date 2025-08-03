0f9f45715651ca291d654058a9784106aa64247e
FrankMilvus <frankmilvus@gmail.com>
2025-08-03 17:40:11 +0300

    error

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.retrofit
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.retrofit.model.JsonPlaceHolderApi
+import com.example.retrofit.model.Post
 import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textViewResult: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,27 +29,53 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val gson = Gson()
 
-        val address=Address("USA","New-York")
-        val employee = Employee("John Doe", 30, "johnDoe@example.com",address)
+        textViewResult = findViewById(R.id.text_view_result)
 
-        val json = gson.toJson(employee)
-        Log.d("json", json)
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi::class.java)
+
+        val call = jsonPlaceHolderApi.getPosts()
+
+        call.enqueue(object : Callback<MutableList<Post>> {
+            override fun onResponse(
+                call: Call<MutableList<Post>>,
+                response: Response<MutableList<Post>>
+            ) {
+                if (response.isSuccessful) {
+
+                    val posts = response.body()!!
+                    for (post in posts) {
+                        var content = ""
+                        content += "ID: " + post.id + "\n"
+                        content += "User ID: " + post.userId + "\n"
+                        content += "Title: " + post.title + "\n"
+                        content += "Text: " + post.text + "\n\n"
+                        textViewResult.append(content)
+                    }
+                } else {
+                    textViewResult.text = response.code().toString()
+
+                }
+            }
 
 
+            override fun onFailure(
+                call: Call<MutableList<Post>>,
+                t: Throwable
+            ) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Something went wrong ${t.toString()}",
+                    Toast.LENGTH_SHORT
+                ).show()
+                textViewResult.text="Some error"
+            }
 
-
-//        {
-//            "age": 30,
-//            "mail": "johnDoe@example.com",
-//            "name": "John Doe"
-//        }
-
-//        val json2="{\"firstName\":\"John\",\"age\":30,\"mail\":\"johnDoe@example.com\",\"name\":\"John Doe\"}"
-//
-//        val employee2 = gson.fromJson(json2, Employee::class.java)
-//        Log.d("json2", json2)
-//        println(json2)
+        })
     }
 }
