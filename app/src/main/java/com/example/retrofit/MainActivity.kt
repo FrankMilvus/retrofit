cca8cdfe39846c58b48dd78856e789d1ff5aa960
FrankMilvus <frankmilvus@gmail.com>
2025-08-03 11:05:22 +0300

    json to and fro

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.retrofit
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val gson = Gson()
+
+        val employee = Employee("John Doe", 30, "johnDoe@example.com")
+
+        val json = gson.toJson(employee)
+
+
+
+
+//        {
+//            "age": 30,
+//            "mail": "johnDoe@example.com",
+//            "name": "John Doe"
+//        }
+
+        val json2="{\"firstName\":\"John\",\"age\":30,\"mail\":\"johnDoe@example.com\",\"name\":\"John Doe\"}"
+
+        val employee2 = gson.fromJson(json2, Employee::class.java)
+        Log.d("json2", json2)
+        println(json2)
     }
 }
---
+++ app/src/main/java/com/example/retrofit/employee.kt
@@ -0,0 +1,5 @@
+package com.example.retrofit
+
+data class Employee(val name: String, val age: Int, val mail: String){
+
+}
